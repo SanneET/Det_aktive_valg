eff5e026b8cc56a6826957fbc5f3df77a83f66e3
SanneET <eaa23set@students.eaaa.dk>
2024-11-07 12:06:15 +0100

    fetch for knap 1

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ const endpoint = 'json/jsondata.json';
 const fetchValgEtBtn = document.getElementById('valgEtBtn');
 
 const fetchValgToBtn = document.getElementById('valgToBtn');
+
+/* Tilføjer en eventListner (lytter) til variablen: fetchValgEtBtn */
+fetchValgEtBtn.addEventListener('click', function(){
+    fetch(endpoint)
+    .then(Response => Response.json()) //konvetere response til JSON-format.
+    .then(jsonData => {
+
+
+    })
+})
