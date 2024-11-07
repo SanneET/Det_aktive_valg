e09ab94ee830ffbd8ac9d5ead678123986b18cd2
leaxnimb <eaa23lnlu@students.eaaa.dk>
2024-11-07 10:53:24 +0100

    Små ændringer

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const endpoint = 'json/jsondata.json';
+
+const fetchValgEtBtn = document.getElementById('valgEtBtn');
+
+const fetchValgToBtn = document.getElementById('valgToBtn');
