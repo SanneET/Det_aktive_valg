155b2ba24f4284255720d99e065bc857b05d3a88
leaxnimb <eaa23lnlu@students.eaaa.dk>
2024-11-07 12:05:59 +0100

    variabler

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 
 const endpoint = 'json/jsondata.json';
 
+/* Erklære en variabel som refererer til knappen med ID'et valgEtBtn */
 const fetchValgEtBtn = document.getElementById('valgEtBtn');
 
+/* Erklære en variabel som refererer til knappen med ID'et valgToBtn */
 const fetchValgToBtn = document.getElementById('valgToBtn');
+
+
+
+
+})
