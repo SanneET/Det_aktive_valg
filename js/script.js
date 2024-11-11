cb5aa3ab1a610564c09618a843b4e0b18ea7f0e4
SanneET <eaa23set@students.eaaa.dk>
2024-11-11 11:03:23 +0100

    oprettelse af video indhold

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@ const fetchValgEtBtn = document.getElementById('valgEtBtn');
 
 const fetchValgToBtn = document.getElementById('valgToBtn');
 
+const videoElement = document.getElementById('video');
+
 /* Tilføjer en eventListner (lytter) til variablen: fetchValgEtBtn */
 fetchValgEtBtn.addEventListener('click', function(){
     fetch(endpoint)
     .then(Response => Response.json()) //konvetere response til JSON-format.
     .then(jsonData => {
+        videoElement.src = jsonData.videoOne;
 
     })});
 
@@ -20,6 +23,7 @@ fetchValgToBtn.addEventListener('click', function(){
     fetch(endpoint)
     .then(Response => Response.json()) //konvetere response til JSON-format.
     .then(jsonData => {
+        videoElement.src = jsonData.videoOne;
 
 
         
