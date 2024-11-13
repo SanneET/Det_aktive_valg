a0e6da1e98258c15ee0e1ad2a96394e0bd0824cc
SanneET <eaa23set@students.eaaa.dk>
2024-11-13 11:09:03 +0100

    update

--- js/delayfirstChoiceTwo.js
+++ js/delayfirstChoiceTwo.js
@@ -1,4 +1,4 @@
 // After 3 seconds (3000 milliseconds), add the 'show' class to the element
 setTimeout(() => {
     document.getElementById("flexId").classList.add("show");
-  }, 37000);
+  }, 47000);
